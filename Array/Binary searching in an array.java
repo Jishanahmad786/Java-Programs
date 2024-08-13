df15062c2e19e3089874d99f1a23a28c7ff12286
Jishan Ahmad <62088833+Jishanahmad786@users.noreply.github.com>
2024-08-13 20:03:06 +0845

    Create Binary searching in an array.java

---
+++ Array/Binary searching in an array.java
@@ -0,0 +1,42 @@
+public class Test4 
+{
+    public static int binarySearch(int num[],int key)
+    {
+    int start=0; int end=num.length-1;
+    while (start<=end)
+    {
+        int mid=(start+end)/2;
+        // comparision
+        if(num[mid]==key)
+        {
+            return mid;
+        }
+        if(num[mid]<key)
+        {
+            start=mid+1;
+        }
+        else
+        {
+            end=mid-1;
+        }
+    }
+    return 0;
+        
+    }
+    public static void main(String[] args)
+    {
+        int num[]={2,4,6,8,10,12,14};
+        int key=10;
+       int index= binarySearch(num, key);
+       if(index==0)
+       {
+        System.out.println("Number not found");
+       }
+       else
+       {
+        System.out.println("Number at index"+index);
+       }
+        
+    }
+    
+}
